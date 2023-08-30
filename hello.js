6c850e37bcca7574d3c1a9381f3ec7dd75b0bd42
nfawad <nofil.fawad@intel.com>
2023-08-29 20:28:54 -0700

    Message updated

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World How are you - Salud from Nofil";
 console.log(hello());
 exports.hello = hello;
